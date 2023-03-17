14c61cc6f8c0305b24efe0e3e93e207982c857d5
acsanti92 <armando.santiago@pluto.tv>
2023-03-17 11:55:06 -0700

    Added Virtual Camera for Aiming Functionality

---
+++ just-us/Assets/Scripts/ThirdPersonShooterController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    // Cinemachine Virtual Camera that allows us to aim
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+
+    private void Update()
+    {
+
+    }
+}
